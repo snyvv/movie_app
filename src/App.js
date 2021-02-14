51e387d88ac870d5e47f5a33b60a58826863ae5e
김신영/프론트개발팀 <ksy@musinsa.com>
2021-02-14 18:37:14 +0900

    Step8 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
 import './App.scss';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies: [],
-  }
-
-  getMovies = async () => { // async(비동기) > axios.get()을 실행하려면 시간이 필요함
-    const {
-      data: {
-        data : { movies },
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // await > getMovies() 함수 내부의 axios.get()의 실행 완료를 기다렸다가 끝나면 계속 진행
-    this.setState({ movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading 
-          ? (
-            <div className="loader">Loading...</div>
-          )
-          : (
-            <div className="movie">
-              {movies.map((movie) => {
-              return <Movie 
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />; 
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  )
 }
 
 export default App;
